2a402c09aae418f4de64fcced7b4118c13c77b9d
marku <markus.hauschild.negativ@googlemail.com>
2017-10-08 12:38:37 +0200

    init

---
+++ app/src/main/java/com/mahausch/couchmovies/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mahausch.couchmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
