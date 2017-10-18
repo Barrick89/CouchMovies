894de6a0e361f0fba7a82a649fb62a19a4ff4d29
marku <markus.hauschild.negativ@googlemail.com>
2017-10-18 20:37:13 +0200

    Simplify the list preference summary

--- app/src/main/java/com/mahausch/couchmovies/SettingsActivity.java
+++ app/src/main/java/com/mahausch/couchmovies/SettingsActivity.java
@@ -15,35 +15,13 @@ public class SettingsActivity extends Activity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+    public static class SettingsFragment extends PreferenceFragment  {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
 
-            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
-            bindPreferenceSummaryToValue(orderBy);
         }
-
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(stringValue);
-            if (prefIndex >= 0) {
-                CharSequence[] labels = listPreference.getEntries();
-                preference.setSummary(labels[prefIndex]);
-            }
-
-            return true;
-        }
-
-        private void bindPreferenceSummaryToValue(Preference preference) {
-            preference.setOnPreferenceChangeListener(this);
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
-            String preferenceString = preferences.getString(preference.getKey(), "");
-            onPreferenceChange(preference, preferenceString);
-        }
-
     }
 }
