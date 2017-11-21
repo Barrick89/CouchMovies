5c9b7d8bcc59edde68a6cfee93e63a3d4cba1f4d
marku <markus.hauschild.negativ@googlemail.com>
2017-11-21 20:44:04 +0100

    Add MovieProvider

---
+++ app/src/main/java/com/mahausch/couchmovies/data/MovieProvider.java
@@ -0,0 +1,121 @@
+package com.mahausch.couchmovies.data;
+
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class MovieProvider extends ContentProvider {
+
+    public static final int CODE_MOVIES = 100;
+    public static final int CODE_MOVIE_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDbHelper mOpenHelper;
+
+    public static UriMatcher buildUriMatcher(){
+
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY, MovieContract.PATH_MOVIE, CODE_MOVIES);
+        matcher.addURI(MovieContract.CONTENT_AUTHORITY, MovieContract.PATH_MOVIE + "/#", CODE_MOVIE_WITH_ID);
+        return matcher;
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        Cursor cursor;
+
+        switch (sUriMatcher.match(uri)){
+
+            case CODE_MOVIE_WITH_ID:
+                selection = MovieContract.MovieEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                break;
+
+            case CODE_MOVIES:
+                selection = MovieContract.MovieEntry.COLUMN_IMAGE + "=?";
+        }
+
+        cursor = mOpenHelper.getReadableDatabase().query(
+                MovieContract.MovieEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+
+        switch (sUriMatcher.match(uri)) {
+
+            case CODE_MOVIE_WITH_ID:
+
+                long id = mOpenHelper.getWritableDatabase().
+                        insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+
+                if (id == -1) {
+                    Log.e("Insert", "Failed to insert row for " + uri);
+                    return null;
+                }
+
+                getContext().getContentResolver().notifyChange(uri, null);
+                return ContentUris.withAppendedId(uri, id);
+        }
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        switch (sUriMatcher.match(uri)){
+
+            case CODE_MOVIE_WITH_ID:
+                selection = MovieContract.MovieEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+        }
+
+        int rowsDeleted = mOpenHelper.getWritableDatabase().delete(
+                MovieContract.MovieEntry.TABLE_NAME,
+                selection,
+                selectionArgs
+        );
+
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
