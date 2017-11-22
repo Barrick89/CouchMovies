505cfe71ed9f623b04b09e18a6730b18adda1248
marku <markus.hauschild.negativ@googlemail.com>
2017-11-22 20:52:00 +0100

    Add Cursor Loader in MainActivity

--- app/src/main/java/com/mahausch/couchmovies/MainActivity.java
+++ app/src/main/java/com/mahausch/couchmovies/MainActivity.java
@@ -1,10 +1,13 @@
 package com.mahausch.couchmovies;
 
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -21,12 +24,13 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.mahausch.couchmovies.data.MovieContract;
 import com.mahausch.couchmovies.utilities.NetworkUtils;
 
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler, LoaderManager.LoaderCallbacks<ArrayList<Movie>> {
+public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler {
 
     RecyclerView mRecyclerView;
     MovieAdapter mAdapter;
@@ -59,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         startTaskLoader();
 
+
         final SwipeRefreshLayout swipe = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
 
         swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -80,53 +85,135 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         startActivity(intent);
     }
 
-    @Override
-    public Loader<ArrayList<Movie>> onCreateLoader(int id, final Bundle args) {
 
-        return new AsyncTaskLoader<ArrayList<Movie>>(this) {
+    private LoaderManager.LoaderCallbacks<ArrayList<Movie>> mLoaderCallbacksInternet = new LoaderManager.LoaderCallbacks<ArrayList<Movie>>() {
 
-            @Override
-            protected void onStartLoading() {
-                super.onStartLoading();
+        @Override
+        public Loader<ArrayList<Movie>> onCreateLoader(int id, Bundle args) {
+
+            return new AsyncTaskLoader<ArrayList<Movie>>(getBaseContext()) {
+
+                @Override
+                protected void onStartLoading() {
+                    super.onStartLoading();
 
 
-                mProgress.setVisibility(View.VISIBLE);
-                forceLoad();
-            }
-
-            @Override
-            public ArrayList<Movie> loadInBackground() {
-
-                ArrayList<URL> movieResponseURLs = NetworkUtils.buildUrl(mPreference);
-
-                try {
-                    ArrayList<String> jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(movieResponseURLs);
-
-                    return NetworkUtils.getMovieDataFromJson(jsonMovieResponse);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
+                    mProgress.setVisibility(View.VISIBLE);
+                    forceLoad();
                 }
-            }
-        };
-    }
 
-    @Override
-    public void onLoadFinished(Loader<ArrayList<Movie>> loader, ArrayList<Movie> data) {
-        mProgress.setVisibility(View.INVISIBLE);
-        if (data != null) {
-            mAdapter.setMovieData(data);
-        } else {
-            Toast toast = Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT);
-            toast.show();
+                @Override
+                public ArrayList<Movie> loadInBackground() {
+
+                    ArrayList<URL> movieResponseURLs = NetworkUtils.buildUrl(mPreference);
+
+                    try {
+                        ArrayList<String> jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(movieResponseURLs);
+
+                        return NetworkUtils.getMovieDataFromJson(jsonMovieResponse);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+            };
         }
-    }
 
-    @Override
-    public void onLoaderReset(Loader<ArrayList<Movie>> loader) {
+        @Override
+        public void onLoadFinished(Loader<ArrayList<Movie>> loader, ArrayList<Movie> data) {
+            mProgress.setVisibility(View.INVISIBLE);
+            if (data != null) {
+                mAdapter.setMovieData(data);
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
 
-    }
+        @Override
+        public void onLoaderReset(Loader<ArrayList<Movie>> loader) {
+
+        }
+    };
+
+    private LoaderManager.LoaderCallbacks<Cursor> mLoaderCallbacksFavorites = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+            return new AsyncTaskLoader<Cursor>(getBaseContext()) {
+
+
+                @Override
+                protected void onStartLoading() {
+                    super.onStartLoading();
+
+                    mProgress.setVisibility(View.VISIBLE);
+                    forceLoad();
+                }
+
+                @Override
+                public Cursor loadInBackground() {
+
+                    Uri currentUri = MovieContract.MovieEntry.CONTENT_URI;
+
+                    String[] projection = new String[]{
+                            MovieContract.MovieEntry._ID,
+                            MovieContract.MovieEntry.COLUMN_MOVIE_ID,
+                            MovieContract.MovieEntry.COLUMN_TITLE,
+                            MovieContract.MovieEntry.COLUMN_DATE,
+                            MovieContract.MovieEntry.COLUMN_RATING,
+                            MovieContract.MovieEntry.COLUMN_IMAGE,
+                            MovieContract.MovieEntry.COLUMN_CONTENT
+                    };
+                    return getContentResolver().query(currentUri, projection, null, null, null);
+                }
+
+            };
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+            if (data == null) {
+
+                ArrayList<Movie> list = new ArrayList<>();
+                data.moveToFirst();
+
+                int movieIdIndex = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_MOVIE_ID);
+                int titleIndex = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_TITLE);
+                int dateIndex = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_DATE);
+                int ratingIndex = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_RATING);
+                int imageIndex = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_IMAGE);
+                int contentIndex = data.getColumnIndex(MovieContract.MovieEntry.COLUMN_CONTENT);
+
+                do {
+                    int movieId = data.getInt(movieIdIndex);
+                    String title = data.getString(titleIndex);
+                    String date = data.getString(dateIndex);
+                    double rating = data.getDouble(ratingIndex);
+                    String image = data.getString(imageIndex);
+                    String content = data.getString(contentIndex);
+
+                    Movie movie = new Movie(image, title, movieId, date, content, rating);
+                    list.add(movie);
+                } while (data.moveToNext());
+
+                mProgress.setVisibility(View.INVISIBLE);
+
+                mAdapter.setMovieData(list);
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -164,19 +251,24 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     public void startTaskLoader() {
 
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnected()) {
-            mProgress.setVisibility(View.VISIBLE);
-
-            getSupportLoaderManager().restartLoader(1, null, this);
-
+        if (mPreference.equals("favorites")) {
+            getSupportLoaderManager().restartLoader(2, null, mLoaderCallbacksFavorites);
         } else {
-            mProgress.setVisibility(View.INVISIBLE);
-            Toast toast = Toast.makeText(this, R.string.network_error, Toast.LENGTH_SHORT);
-            toast.show();
+
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isConnected()) {
+                mProgress.setVisibility(View.VISIBLE);
+
+                getSupportLoaderManager().restartLoader(1, null, mLoaderCallbacksInternet);
+
+            } else {
+                mProgress.setVisibility(View.INVISIBLE);
+                Toast toast = Toast.makeText(this, R.string.network_error, Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
     }
 }
